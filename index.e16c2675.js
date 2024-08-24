7c89c135453bbdfe6dad20f02a1980bf4554de1f
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-08-24 10:35:59 +0300

    make build

---
+++ index.e16c2675.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");if(r){var a=r.getAttribute("href");e.src=a}});
+//# sourceMappingURL=index.e16c2675.js.map
